c322dcedb1d597ad8ee47c8cf99790eebfd67a87
n04x <thomasbacks@gmail.com>
2021-07-15 18:13:29 -0400

    prediction stock is working...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.lib.type_check import real
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
@@ -11,10 +12,10 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.python.keras.engine import input_spec
 
 # Load the data
-company = 'AAPL'
+company = 'TSLA'
 
 start = dt.datetime(2012,1,1)
-end = dt.datetime(2020,1,1)
+end = dt.datetime(2021,1,1)
 
 # data = web.DataReader(company, 'alphavantage', start, end)
 # data = web.get_data_alphavantage(company, start=start, end=end)
@@ -49,18 +50,19 @@ model.add(Dropout(0.2))
 model.add(Dense(units=1)) # Prediciton of the next closing market value
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, initial_epoch=25, batch_size=32)
+model.fit(x_train, y_train, epochs=25, batch_size=32)
 
 ''' Test the model accuracy on existing data, the training test'''
 
 # Load Test Data
-test_start = dt.datetime(2020,1,1)
+test_start = dt.datetime(2021,1,1)
 test_end = dt.datetime.now()
 
-test_data = web.DataReader(company, 'yahoo', test_start, test_end)
-actual_prices = test_data['Close'].values
+test_data = web.DataReader(company, "av-daily", start=start, end=end, api_key='YNSYV764TKT1EHPH')
+print(test_data)
+actual_prices = test_data['close'].values
 
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+total_dataset = pd.concat((data['close'], test_data['close']), axis=0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
 model_inputs = model_inputs.reshape(-1, 1)
@@ -74,16 +76,27 @@ for x in range(prediction_days, len(model_inputs)):
     x_test.append(model_inputs[x-prediction_days:x, 0])
 
 x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1]), 1)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
+
+# Predict Next Day
+
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f'Prediction: {prediction}')
 
 # Plot the test predictions
 plt.plot(actual_prices, color="black", label=f"Actual {company} price")
 plt.plot(predicted_prices, color="green", label=f"Predicted {company} price")
 plt.title(f"{company} share price")
-plt.xlabel('time')
+plt.xlabel('Time')
 plt.ylabel(f'{company} Share Price')
 plt.legend()
 plt.show()
+
